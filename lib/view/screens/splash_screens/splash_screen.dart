5a51443ebb12e9039a08fccb09a4e9e87f429438
AbdurRehman-coder <abdurrehmanlkl786@gmail.com>
2023-05-05 23:11:09 +0500

    working on splash screen animation

--- lib/view/screens/splash_screens/splash_screen.dart
+++ lib/view/screens/splash_screens/splash_screen.dart
@@ -1,5 +1,4 @@
 
-
 import 'package:blinkid/resources/constants/images_path.dart';
 import 'package:blinkid/resources/utils/routes.dart';
 import 'package:flutter/cupertino.dart';
@@ -11,28 +10,201 @@ class SplashScreen extends StatefulWidget {
   @override
   State<SplashScreen> createState() => _SplashScreenState();
 }
+//
+// class _SplashScreenState extends State<SplashScreen> {
+//
+//   @override
+//   void initState() {
+//     // TODO: implement initState
+//     super.initState();
+//     Future.delayed(Duration(seconds: 5), (){
+//       Navigator.pushNamed(context, Routes.locationAccessScreen);
+//     });
+//
+//   }
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       body: Container(
+//         height: MediaQuery.of(context).size.height,
+//         width: MediaQuery.of(context).size.width,
+//         decoration: const BoxDecoration(
+//         ),
+//         child: Image.asset(AppImages.splashImage,  fit: BoxFit.cover,),
+//       ),
+//     );
+//   }
+// }
 
-class _SplashScreenState extends State<SplashScreen> {
-  
+
+
+/// Scale Animation
+// class _SplashScreenState extends State<SplashScreen> with SingleTickerProviderStateMixin {
+//   late AnimationController _controller;
+//   late Animation<double> _animation;
+//
+//   @override
+//   void initState() {
+//     super.initState();
+//     _controller = AnimationController(
+//       duration: const Duration(seconds: 4),
+//       vsync: this,
+//     )..forward();
+//     _animation = Tween<double>(
+//       begin: 0.5,
+//       end: 1.0,
+//     ).animate(CurvedAnimation(
+//       parent: _controller,
+//       curve: Curves.easeInOut,
+//     ));
+//     _controller.addStatusListener((status) {
+//       if (status == AnimationStatus.completed) {
+//         Navigator.pushNamed(context, Routes.locationAccessScreen);
+//       }
+//     });
+//   }
+//
+//   @override
+//   void dispose() {
+//     _controller.dispose();
+//     super.dispose();
+//   }
+//
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       body: Container(
+//         height: MediaQuery.of(context).size.height,
+//         width: MediaQuery.of(context).size.width,
+//         decoration: const BoxDecoration(),
+//         child: ScaleTransition(
+//           scale: _animation,
+//           child: Image.asset(
+//             AppImages.splashImage,
+//             fit: BoxFit.cover,
+//           ),
+//         ),
+//       ),
+//     );
+//   }
+// }
+
+/// Fade Animation
+// class _SplashScreenState extends State<SplashScreen> with SingleTickerProviderStateMixin {
+//   late AnimationController _controller;
+//   late Animation<double> _animation;
+//
+//   @override
+//   void initState() {
+//     super.initState();
+//     _controller = AnimationController(
+//       duration: const Duration(seconds: 3),
+//       vsync: this,
+//     )..forward();
+//     _animation = Tween<double>(
+//       begin: 0.0,
+//       end: 1.0,
+//     ).animate(CurvedAnimation(
+//       parent: _controller,
+//       curve: Curves.easeInOut,
+//     ));
+//     _controller.addStatusListener((status) {
+//       if (status == AnimationStatus.completed) {
+//         Navigator.pushNamed(context, Routes.locationAccessScreen);
+//       }
+//     });
+//   }
+//
+//   @override
+//   void dispose() {
+//     _controller.dispose();
+//     super.dispose();
+//   }
+//
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       body: Container(
+//         height: MediaQuery.of(context).size.height,
+//         width: MediaQuery.of(context).size.width,
+//         decoration: const BoxDecoration(),
+//         child: FadeTransition(
+//           opacity: _animation,
+//           child: Image.asset(
+//             AppImages.splashImage,
+//             fit: BoxFit.cover,
+//           ),
+//         ),
+//       ),
+//     );
+//   }
+// }
+
+
+
+/// Slide Animation
+class _SplashScreenState extends State<SplashScreen> with SingleTickerProviderStateMixin {
+
+  late AnimationController _animationController;
+  late Animation<Offset> _slideAnimation;
+
   @override
   void initState() {
-    // TODO: implement initState
     super.initState();
-    Future.delayed(Duration(seconds: 5), (){
-      Navigator.pushNamed(context, Routes.locationAccessScreen);
-    });
-    
+    _animationController = AnimationController(
+      vsync: this,
+      duration: Duration(seconds: 3),
+    );
+
+
+    _slideAnimation = Tween<Offset>(
+      begin: Offset.zero,
+      end: const Offset(0.0, 1.0),
+    ).animate(_animationController);
+
+    _animationController.forward().then(
+          (_) => Navigator.pushNamed(context, Routes.locationAccessScreen),
+    );
+
   }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Container(
-        height: MediaQuery.of(context).size.height,
-        width: MediaQuery.of(context).size.width,
-        decoration: const BoxDecoration(
-        ),
-        child: Image.asset(AppImages.splashImage,  fit: BoxFit.cover,),
+      body: Stack(
+        children: [
+          Container(
+            height: MediaQuery.of(context).size.height,
+            width: MediaQuery.of(context).size.width,
+            decoration: const BoxDecoration(),
+            child: Image.asset(
+              AppImages.splashImage,
+              fit: BoxFit.cover,
+            ),
+          ),
+          SlideTransition(
+            position: _slideAnimation,
+            child: Container(
+              height: MediaQuery.of(context).size.height,
+              width: MediaQuery.of(context).size.width,
+              color: Colors.white,
+              child: Center(
+                child: Image.asset(
+                  AppImages.splashImage,
+                  fit: BoxFit.cover,
+                ),
+              ),
+            ),
+          ),
+        ],
       ),
     );
   }
+
+  @override
+  void dispose() {
+    _animationController.dispose();
+    super.dispose();
+  }
 }
+
